ac57d8edbda941ab7b6f122a4fe7b84554cab8ac
maicol <maicol.matteoli@gmail.com>
2021-05-28 14:25:48 +0200

    add : onclick

--- js/script.js
+++ js/script.js
@@ -1,21 +1,23 @@
 // var rand = Math.floor(Math.random() * 6); 
 // console.log(rand);
 
-//genero due random tra 1 e 6 compresi
-var randUtente = Math.floor(Math.random() * 6 + 1);
-var randPC = Math.floor(Math.random() * 6 + 1);
+function randoms(){
+    //genero due random tra 1 e 6 compresi
+    var randUtente = Math.floor(Math.random() * 6 + 1);
+    var randPC = Math.floor(Math.random() * 6 + 1);
 
-console.log("PC : " + randPC);
-console.log("Utente : " + randUtente);
+    console.log("PC : " + randPC);
+    console.log("Utente : " + randUtente);
 
 
-//confronto valori 
-if(randPC > randUtente){
-    console.log("PC vince :" + randPC);
+    //confronto valori 
+    if(randPC > randUtente){
+        console.log("PC vince :" + randPC);
+    }
+    else if(randPC < randUtente){
+            console.log("Utente Vince : " + randUtente);
+        }
+        else{
+            console.log("pareggio");
+        }
 }
-else if(randPC < randUtente){
-        console.log("Utente Vince : " + randUtente);
-    }
-    else{
-        console.log("pareggio");
-    }
