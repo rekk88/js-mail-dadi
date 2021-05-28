6c34c5db1399572da148f337e900be7535b88d27
maicol <maicol.matteoli@gmail.com>
2021-05-28 14:11:27 +0200

    add : generazione di due random tra 1 e 6

--- js/script.js
+++ js/script.js
@@ -1,2 +1,6 @@
-var rand = Math.floor(Math.random() * 6); 
-console.log(rand);
+// var rand = Math.floor(Math.random() * 6); 
+// console.log(rand);
+
+//genero due random tra 1 e 6 compresi
+var randUtente = Math.floor(Math.random() * 6 + 1);
+var randPC = Math.floor(Math.random() * 6 + 1);
