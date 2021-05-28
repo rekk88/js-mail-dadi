d470e31cda20ac2b4029de1ad66bf709822c83c1
maicol <maicol.matteoli@gmail.com>
2021-05-28 14:13:42 +0200

    add: visualizzazione vincitore e confronto valori

--- js/script.js
+++ js/script.js
@@ -4,3 +4,18 @@
 //genero due random tra 1 e 6 compresi
 var randUtente = Math.floor(Math.random() * 6 + 1);
 var randPC = Math.floor(Math.random() * 6 + 1);
+
+console.log("PC : " + randPC);
+console.log("Utente : " + randUtente);
+
+
+//confronto valori 
+if(randPC > randUtente){
+    console.log("PC vince :" + randPC);
+}
+else if(randPC < randUtente){
+        console.log("Utente Vince : " + randUtente);
+    }
+    else{
+        console.log("pareggio");
+    }
